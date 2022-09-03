c1177744d41a71d327b44e149f986b8798f9444d
BlackMonkey-1219 <110360091+BlackMonkey-1219@users.noreply.github.com>
2022-09-03 11:31:20 +0530

    Data validation script(inspector.py): v1

---
+++ inspecter.py
@@ -0,0 +1,15 @@
+import re
+import json
+import argparse
+import sys
+
+# ==============================================================================================
+INPUT_FILE_NAME = None
+DATA_OBJ = {}
+# ==============================================================================================
+
+# LOAD JSON FILE INTO A PYTHON OBJECT
+# ITERATE THROUGH ITEMS
+# REMOVE UNNECESORY CHARS
+# FORMAT DATA ON A TEMPLATE
+# REWRITE THE DATA INTO THE FILE
