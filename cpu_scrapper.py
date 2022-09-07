be8eba806318533c86a753df3817f9bab6819282
BlackMonkey-1219 <110360091+BlackMonkey-1219@users.noreply.github.com>
2022-09-08 03:14:53 +0530

    Minor Adjustments
    Commented console output commands.
    Now can add URLs
    Can Separate Cores from Threads
    Data writing happens in the very end.
    ROW dict now stores empty strings instead of None object.

--- cpu_scrapper.py
+++ cpu_scrapper.py
@@ -30,9 +30,11 @@ def extract_links(page: BS, filter: str = "") -> dict:
         except Exception as E:
             print(f"[-] LINK EXTRACTION ERROR: {E}")
     
+    print('=' * 100)
     keys = list(LINKS.keys())
     for i in range(len(keys)):
         print(f"{[i]} => {keys[i]}")
+    print('=' * 100)
 
     indexes = input("Choose the Links You Want: ").split(' ')
 
@@ -54,7 +56,7 @@ def crawler(URL: str, ELEMENT: str) -> dict:
     page = BS(pageBuffer.content, "lxml")
 
     elements = page.find_all(ELEMENT)
-    print(f"{len(elements)} {ELEMENT} FOUND")
+    print(f"[i] {len(elements)} {ELEMENT} FOUND")
 
     # ASKING IF THE USER WANT TO CONTINUE THE CRAWLING
     prompt = input("Do you want to continue (Y/N)?: ")
@@ -68,9 +70,9 @@ def crawler(URL: str, ELEMENT: str) -> dict:
         for table in tables:
             TABLE = {}
 
-            print('=' * 50)
-            print(f"TABLE: {tables.index(table)}")
-            print('=' * 50)
+            #print('=' * 50)
+            print(f"[i] TABLE: {tables.index(table)}")
+            #print('=' * 50)
 
             # GET EVERY TABLE ROW IN THE TABLE
             tableRows = table.find_all('tr')
@@ -78,7 +80,8 @@ def crawler(URL: str, ELEMENT: str) -> dict:
 
             # LOOPING THOURGH FIRST 2 ROWS TO FIND HEADERS OF THE TABLE
             for tableRow in tableRows[0:2]:
-                print(f"SCANNING HEADING ROW: {tableRows.index(tableRow)}")
+                print(f"[i] SCANNING HEADING ROW: {tableRows.index(tableRow)}")
+                #print(f"[i] HEADINGS ROW: {BS.prettify(tableRow)}")
 
                 try:
                     headingElements = tableRow.find_all("th")
@@ -90,13 +93,13 @@ def crawler(URL: str, ELEMENT: str) -> dict:
                 except:
                     pass
             
-            print(headers)
+            #print(headers)
 
             HEADING_INDEXES = {
                 "MODEL": None,
                 "FREQUENCY": None,
                 "CORES": None,
-                "THREADS": None
+                "THREADS": None,
             }
 
             # LOOPING THORUGH FOUND HEADERS TO GET THE INDEXES OF THE REQUIRED TABLE COLUMNS
@@ -104,30 +107,34 @@ def crawler(URL: str, ELEMENT: str) -> dict:
                 for heading in headingsRow:
                     if("Model" in heading):
                         index = headers.index(headingsRow) + headingsRow.index(heading)
-                        print(f"MODEL HEADING INDEX: {index}")
+                        print(f"[i] MODEL HEADING INDEX: {index}")
                         HEADING_INDEXES["MODEL"] = index
-                    elif("Fre" in heading):
+                    elif("Fre" in heading or "Clock" in heading):
                         index = headers.index(headingsRow) + headingsRow.index(heading)
-                        print(f"FREQUENCY HEADING INDEX: {index}")
+                        print(f"[i] FREQUENCY HEADING INDEX: {index}")
                         HEADING_INDEXES["FREQUENCY"] = index
                     elif("Cores" in heading):
                         index = headers.index(headingsRow) + headingsRow.index(heading)
-                        print(f"CORES HEADING INDEX: {index}")
+                        print(f"[i] CORES HEADING INDEX: {index}")
                         HEADING_INDEXES["CORES"] = index
                         if("(threads)" in heading):
                             index = headers.index(headingsRow) + headingsRow.index(heading)
-                            print(f"THREADS HEADING INDEX: {index}")
+                            print(f"[i] THREADS HEADING INDEX: {index}")
                             HEADING_INDEXES["THREADS"] = index
                     elif("Threads)" in heading):
                         index = headers.index(headingsRow) + headingsRow.index(heading)
-                        print(f"THREADS HEADING INDEX: {index}")
+                        print(f"[i] THREADS HEADING INDEX: {index}")
                         HEADING_INDEXES["THREADS"] = index
             
-            print('=' * 50)
-            print("HEADING INDEXES")
-            print('=' * 50)
-            print(json.dumps(HEADING_INDEXES, indent=4))
-            print('=' * 50)
+            if(HEADING_INDEXES["MODEL"] == "None" or HEADING_INDEXES["FREQUENCY"] == None):
+                print(f"[-] SKIPPING TABLE {tables.index(table)} DUE TO MISSING DATA")
+                continue
+
+            #print('=' * 50)
+            #print("HEADING INDEXES")
+            #print('=' * 50)
+            #print(json.dumps(HEADING_INDEXES, indent=4))
+            #print('=' * 50)
 
             # CHOOSING WHAT TABLE ROWS TO GO THORUGH DEPENDING ON THE HEADING ROWS FOUND
             if(len(headers) == 1):
@@ -139,44 +146,65 @@ def crawler(URL: str, ELEMENT: str) -> dict:
             
             # LOOPING THOURGH REST OF THE TABLE ROWS TO GET THE DATA
             for tableRow in selectedTableRows:
+                #print(f"TABLE ROW: {BS.prettify(tableRow)}")
+                print('=' * 100)
+                print(f"ROW: {tableRows.index(tableRow)}")
+                #print('=' * 50)
                 ROW = {
-                    "MODEL": None,
-                    "FREQUENCY": None,
-                    "CORES": None,
-                    "THREADS": None,
+                    "MODEL": "",
+                    "FREQUENCY": "",
+                    "CORES": "",
+                    "THREADS": "",
                 }
 
                 # PULLING EVERY TABLE DATA ELEMENT IN THE ROW
-                tableData = tableRow.find_all('td')
+                tableDataElements = tableRow.find_all('td')
+                if(len(tableDataElements) <= 1):
+                    continue
 
                 # PULLING THE TEXT OUT OF THE SELECTED TABLE DATA ELEMETNS
                 for property, index in HEADING_INDEXES.items():
                     try:
-                        ROW[property] = tableData[index].text.strip()
-                    except:
-                        pass
+                        if(property == "CORES"):
+                            value = tableDataElements[index].text.strip().split('  ')[0]
+                            print(f"[+] {property}: {value}")
+                            ROW[property] = value
+                        elif(property == "THREADS"):
+                            value = tableDataElements[index].text.strip().split('  ')[1].replace('(', '').replace(')', '')
+                            print(f"[+] {property}: {value}")
+                            ROW[property] = value
+                        else:
+                            value = re.sub(r"&nbsp[GgMm][Hh]z", "", tableDataElements[index].text.strip())
+                            print(f"[+] {property}: {value}")
+                            ROW[property] = value
+                    except Exception as E:
+                        print(f"[-] DATA PULLING ERROR: {E}")
 
                 TABLE[f"ROW {tableRows.index(tableRow)}"] = ROW
 
-                print('=' * 50)
-                print(f"ROW: {tableRows.index(tableRow)}")
-                print('=' * 50)
-                print(json.dumps(ROW, indent=4))
-                print('=' * 50)
+                #print('=' * 50)
+                #print(f"ROW: {tableRows.index(tableRow)}")
+                #print('=' * 50)
+                #print(json.dumps(ROW, indent=4))
+                print('=' * 100)
 
             TABLES[f"TABLE {tables.index(table)}"] = TABLE
 
-    print('=' * 50)
-    print(json.dumps(TABLES, indent=4))
-    print('=' * 50)
+    #print('=' * 50)
+    #print(json.dumps(TABLES, indent=4))
+    #print('=' * 50)
 
     return TABLES
 
-
 def main():
     WIKI_PAGE_URL = "https://en.wikipedia.org/wiki/Category:Lists_of_microprocessors"
+    URL = input("PASTE URL: ")
 
-    wikiPageBffer = R.get(WIKI_PAGE_URL)
+    if (not URL):
+        wikiPageBffer = R.get(WIKI_PAGE_URL)
+    elif (URL):
+        wikiPageBffer = R.get(URL)
+
     wikiPageBffer.encoding = "UTF-8"
 
     wikiPage = BS(wikiPageBffer.content, "lxml")
@@ -186,11 +214,15 @@ def main():
 
     elementToFind = input("What ELEMENT to Find: ")
 
-    for link in list(links.values()):
-        result = crawler(link, elementToFind)
-        with open("data.json", "a+") as fp:
-            fp.write(str(json.dumps(result, indent=4)))
-            fp.close()
+    DATA = {}
+    PAGES = list(links.keys())
+    for page in PAGES:
+        result = crawler(links[page], elementToFind)
+        DATA[page] = result
+
+    with open("./data/data.json", "w") as fp:
+        fp.write(str(json.dumps(DATA, indent=4)))
+        fp.close()
 
 if __name__ == "__main__":
     main()
