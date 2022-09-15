d1a189a39606cff1819e2c2365e6f97e3e38e3fb
BlackMonkey-1219 <110360091+BlackMonkey-1219@users.noreply.github.com>
2022-09-15 11:00:56 +0530

    table scrapper initialization

---
+++ table_scrapper.py
@@ -0,0 +1,26 @@
+def get_page():
+    pass
+
+def get_tables():
+    pass
+
+def get_rows():
+    pass
+
+def get_columns():
+    pass
+
+def create_virtual_table():
+    pass
+
+def fill_virtula_table():
+    pass
+
+def print_virtual_table():
+    pass
+
+def export_data():
+    pass
+
+def write_to_file():
+    pass
